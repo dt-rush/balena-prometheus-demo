6bba75b739b00804c9ba881d5c22b53a9c856259
dt-rush <nickp@resin.io>
2018-10-19 05:25:27 -0400

    renamed 'blend' to distribution in api mock request-pressure

--- modules/balena-api-mock/src/request-pressure.ts
+++ modules/balena-api-mock/src/request-pressure.ts
@@ -1,7 +1,7 @@
 import * as r from 'request-promise';
 
-// several possible load patterns to blur between
-const patterns = [
+// several possible load distributions to blur between
+const distributions = [
 	[2.0, 0.8],
 	[3.0, 0.5],
 	[3.0, 0.8],
@@ -11,15 +11,15 @@ const patterns = [
 	[5.0, 0.8],
 ];
 
-const randomBlend = () => patterns[Math.round(Math.random() * (patterns.length - 1))];
+const randomDistribution = () => distributions[Math.round(Math.random() * (distributions.length - 1))];
 
-const patternPeriod = 5 * 60 * 1000;
-const patternBlendTargetPeriod = 1 * 60 * 1000;
+const distributionPeriod = 5 * 60 * 1000;
+const distributionBlendTargetPeriod = 1 * 60 * 1000;
 const blendsPerTarget = 4;
 const doRequestPeriod = 50;
 
 export class RequestPressure {
-	pattern : number[]
+	distribution : number[]
 	blendTarget : number[]
 	intervals : NodeJS.Timer[]
 
@@ -28,11 +28,11 @@ export class RequestPressure {
 	}
 
 	start() {
-		this.pattern = randomBlend();
-		this.blendTarget = randomBlend();
-		this.intervals.push(this.patternInterval());	
-		this.intervals.push(this.patternBlendTargetInterval());	
-		this.intervals.push(this.patternDoBlendInterval());	
+		this.distribution = randomDistribution();
+		this.blendTarget = randomDistribution();
+		this.intervals.push(this.distributionInterval());	
+		this.intervals.push(this.distributionBlendTargetInterval());	
+		this.intervals.push(this.distributionDoBlendInterval());	
 		this.intervals.push(this.doRequestInterval());	
 	}
 
@@ -40,24 +40,24 @@ export class RequestPressure {
 		this.intervals.map((interval) => clearInterval(interval));
 	}
 
-	patternInterval = () => setInterval(() => {
-		this.pattern = randomBlend();
-	}, patternPeriod)
+	distributionInterval = () => setInterval(() => {
+		this.distribution = randomDistribution();
+	}, distributionPeriod)
 
-	patternBlendTargetInterval = () => setInterval(() => {
-		this.blendTarget = randomBlend();
-	}, patternBlendTargetPeriod)
+	distributionBlendTargetInterval = () => setInterval(() => {
+		this.blendTarget = randomDistribution();
+	}, distributionBlendTargetPeriod)
 
-	patternDoBlendInterval = () => setInterval(() => {
-		this.pattern = [
-			(this.pattern[0] + this.blendTarget[0]) / 2,
-			(this.pattern[1] + this.blendTarget[1]) / 2,
+	distributionDoBlendInterval = () => setInterval(() => {
+		this.distribution = [
+			(this.distribution[0] + this.blendTarget[0]) / 2,
+			(this.distribution[1] + this.blendTarget[1]) / 2,
 		];
-	}, patternBlendTargetPeriod / blendsPerTarget)
+	}, distributionBlendTargetPeriod / blendsPerTarget)
 
 	doRequestInterval = () => setInterval(() => {
 		r(`http://localhost:3000/latency?` +
-			`mu=${this.pattern[0]}&sigma=${this.pattern[1]}`)
+			`mu=${this.distribution[0]}&sigma=${this.distribution[1]}`)
 			.catch(() => null);
 	}, doRequestPeriod)
 }
